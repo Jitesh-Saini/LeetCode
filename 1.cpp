3f1a761cea4fb33e0647d376c7e436addb4e9701
jitesh <iamjiteshsaini2004@gmail.com>
2025-11-06 22:43:32 +0530

    first commit

---
+++ 1.cpp
@@ -0,0 +1,32 @@
+// [1480] Running Sum of 1d Array
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        vector<int> result;    
+        int sum = 0;
+        for (auto x : nums) {
+            sum += x;           
+            result.push_back(sum); 
+        }
+        return result;          
+    }
+};
+
+int main() {
+    Solution sol;
+    vector<int> nums = {1, 2, 3, 4}; // Example input
+
+    vector<int> ans = sol.runningSum(nums);
+
+    cout << "Running Sum: ";
+    for (int x : ans)
+        cout << x << " ";
+    cout << endl;
+
+    return 0;
+}
